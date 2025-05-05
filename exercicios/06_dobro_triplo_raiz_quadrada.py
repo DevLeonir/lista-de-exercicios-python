c9dfbaf6784320337fa94b6ccc981aac3046a5a8
DevLeonir <juniorjoga102011@hotmail.com>
2025-05-05 17:06:13 -0300

    Calcula dobro, triplo e raiz quadrada

---
+++ exercicios/06_dobro_triplo_raiz_quadrada.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número: "))
+
+dobro = numero *2
+triplo = numero *3
+raiz = numero ** 0.5
+
+print(f"O dobro do número {numero} é {dobro}")
+print(f"O triplo do número {numero} é {triplo}")
+print(f"A raiz quadrada de {numero} é {raiz}")
