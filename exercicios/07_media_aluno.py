a8c8b1c9057ad2d2f270d5c9a5d6040c9d0620bf
DevLeonir <juniorjoga102011@hotmail.com>
2025-05-05 17:19:51 -0300

    Calcula média de duas notas

---
+++ exercicios/07_media_aluno.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"Sua primeira nota foi {nota1}.")
+print(f"Sua segunda nota foi {nota2}.")
+print(f"Sua média foi {media}.")
