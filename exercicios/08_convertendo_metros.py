9fdaba50f5f082e3a27e926c0094c807a1943fa2
DevLeonir <juniorjoga102011@hotmail.com>
2025-05-07 18:23:22 -0300

    Cria conversão de metros para cm e mm

---
+++ exercicios/08_convertendo_metros.py
@@ -0,0 +1,7 @@
+metros = float(input("Digite a quantidade de metros: "))
+
+centimetros = metros * 100
+milimetros = metros * 1000
+
+print(f"{int(metros)} metros equivale a {int(centimetros)} centímetros.")
+print(f"{int(metros)} metros equivale a {int(milimetros)} milímetros")
